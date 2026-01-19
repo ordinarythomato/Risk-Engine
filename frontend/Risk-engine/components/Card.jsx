9041334c49821c310fba21f404bf837d2078b299
Samuel Christian <samuelchristian140@gmail.com>
2026-01-19 00:48:27 +0000

    Add nav card and homepage component

---
+++ frontend/Risk-engine/components/Card.jsx
@@ -0,0 +1,10 @@
+export function Card(){
+    return(
+        <div>
+            <img/>
+            <h3>Example</h3>
+            <h1>Example</h1>
+        </div>
+    )
+}
+
---
+++ frontend/Risk-engine/pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import { Card } from '../components/Card.jsx'
+import './homepage.css'
+
+export function Homepage(){
+    return(
+        <div className="card_container">
+            <Card/>
+            <Card/>
+            <Card/>
+        </div>
+    )
+}
+
